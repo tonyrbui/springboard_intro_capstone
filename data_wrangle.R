46722d120523b9fc2b77134f3531817c9598166f
Tony Bui <33678580+tonyrbui@users.noreply.github.com>
2018-08-29 15:14:45 -0700

    Update data_wrangle.R
    Created new date variables

--- data_wrangle.R
+++ data_wrangle.R
@@ -38,7 +38,6 @@ months = (max(data_raw$date) - min(data_raw$date)) / 30
 # Pressure at sea-level is 760 millimeters of mercury (mmHg). Data ranges from 729.3 to 772.3 mmHg.
 # Windspeed ranges between 0 and 14 meters per second (m/s)
 # Visibilty ranges between 1 and 66 kilometers (km)
-summary(data_raw)
 
 # Verify that date observations are 10 minutes apart
 # Create date lag variable that contains preceding datetime value
@@ -57,27 +56,39 @@ head(data_raw)
 # Only appliance energy use had outlier observations that were above 1,000 Wh
 # Typical U.S. household energy use is about 897,000 Wh per month, so 1,000 Wh does not seem unreasonable
 data_boxplot_Wh <- data_raw %>% select(Appliances, lights)
-
 boxplot(data_boxplot_Wh, main = "Energy Use (Wh)")
-
 data_boxplot_T <- data_raw %>% select(T1, T2, T3, T4, T5, T6, T7, T8, T9, T_out, Tdewpoint)
-
 boxplot(data_boxplot_T, main = "Temperature (C)")
-
 data_boxplot_RH <- data_raw %>% select(RH_1, RH_2, RH_3, RH_4, RH_5, RH_6, RH_7, RH_8, RH_9, RH_out)
-
 boxplot(data_boxplot_RH, main = "Relative Humidity %")
-
 data_boxplot_Wh <- data_raw %>% select(Appliances, lights)
-
 boxplot(data_raw$Press_mm_hg, main = "Atmosphere Pressure (mmHg)")
-
 boxplot(data_raw$Windspeed, main = "Windspeed (m/s)")
-
 boxplot(data_raw$Visibility, main = "Visibility (km)")
 
-# Creating month, day, weekday, and time variables
-data_raw <- mutate(date_month = month(date), date_day = day(date), date_wday = wday(date), date_hour = hour(date), date_minute = minute(date))
+# Creating month, day, weekday, hour, and minute variables
+ddata_raw <-
+  data_raw %>% mutate(
+    date_month = month(date),
+    date_day = day(date),
+    date_wday = wday(date),
+    date_hour = hour(date),
+    date_minute = minute(date)
+  )
 
-hour(data_raw$date)
-minute(data_raw$date)
+# Creating time of day variable
+# Night between 00:00 and 05:59
+# Morning between 06:00 and 11:59
+# Afternoon between 12:00 and 18:00
+# Evening between 06:00 and 11:59
+data_raw <- data_raw % >
+  % mutate(date_timeOfDay = ifelse(date_hour < 6, "night", ifelse(
+    date_hour < 12,
+    "morning",
+    ifelse(date_hour < 18, "afternoon", "evening")
+  )))
+
+# Checking for NA or blank values
+data_raw %>% filter(is.na(date_timeOfDay) | date_timeOfDay == '')
+
+
